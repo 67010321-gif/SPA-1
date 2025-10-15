fc4a4561f5b6a569036215b11e8e74dff75ffb68
67010321-gif <67010321@kmitl.ac.th>
2025-10-15 20:55:14 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 101;
+
+app.get(
+    "/",
+    async (req, res) => {
+        res.sendFile(path.join(__dirname, "public", "index.html"));
+    }
+);
+
+app.listen(
+    port,
+    () => {
+        console.log(`Server listening at http://localhost:${port}`);
+    }
+);
